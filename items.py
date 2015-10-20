ebe906d1adfbefc8d2daec5b8c103983ae2e3343
Alex Stanton <alexstanton96@hotmail.co.uk>
2015-10-20 12:48:48 +0100

    First files

---
+++ items.py
@@ -0,0 +1,12 @@
+item_soap = {
+    "id": "soap",
+    "name": "soap",
+    "description": "a bar of soap, watch you dont drop it!"
+}
+
+
+item_coffee = {
+	"id": "coffee",
+	"name": "coffee",
+    "descrition": "a cup of hot coffee"
+}
---
+++ player.py
@@ -0,0 +1,6 @@
+from map import rooms
+from items import *
+
+current_room = rooms["Reception"]
+
+inventory = [item_soap, item_coffee]
